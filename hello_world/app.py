a5e3dd4b1cbab2c64ca36fd98f6c5dfae7cd2409
hasesho05 <midorinotanuki56@gmail.com>
2023-02-07 01:01:22 +0900

    形態素解析追加

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,12 +2,13 @@ import json
 import os
 import requests
 import tweepy
+import collections
 
 
 def lambda_handler(event, context):
 
     try:
-        user_id = event["queryStringParameters"]["id"]
+        query = event["queryStringParameters"]["id"]
     except Exception as e:
         print(str(e))
         return {
@@ -24,24 +25,107 @@ def lambda_handler(event, context):
     auth = tweepy.OAuthHandler(API_KEY, API_ACCESS)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
-    search_words = "python"
-    item_count = 10
+    item_count = 100
 
-    try:
-        tweets = tweepy.Cursor(api.user_timeline, id=user_id).items(item_count)
-    except Exception as e:
-        print(str(e))
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": "hello world"}),
-        }
+    # tweet_list = [
+    #     tweet.text
+    #     for tweet in tweepy.Cursor(api.user_timeline, id=query).items(item_count)
+    #     if (list(tweet.text)[:2] != ["R", "T"]) & (list(tweet.text)[0] != "@")
+    # ]
 
     tweet_list = [
         tweet.text
-        for tweet in tweepy.Cursor(api.user_timeline, id=user_id).items(100)
+        for tweet in tweepy.Cursor(api.search_tweets, q=query, lang="en").items(item_count)
         if (list(tweet.text)[:2] != ["R", "T"]) & (list(tweet.text)[0] != "@")
     ]
 
+    temp_tweet = " ".join(tweet_list)
+    temp_list = temp_tweet.split(" ")
+    remove_list = [
+        query,
+        "a",
+        "to",
+        "for",
+        "in",
+        "of",
+        "the",
+        "and",
+        "is",
+        "on",
+        "I",
+        "you",
+        "it",
+        "that",
+        "this",
+        "with",
+        "at",
+        "from",
+        "by",
+        "are",
+        "as",
+        "be",
+        "have",
+        "or",
+        "an",
+        "will",
+        "my",
+        "can",
+        "not",
+        "but",
+        "was",
+        "what",
+        "your",
+        "all",
+        "about",
+        "there",
+        "if",
+        "when",
+        "how",
+        "up",
+        "out",
+        "so",
+        "some",
+        "he",
+        "she",
+        "they",
+        "me",
+        "we",
+        "us",
+        "our",
+        "their",
+        "them",
+        "his",
+        "her",
+        "their",
+        "its",
+        "am",
+        "do",
+        "does",
+        "did",
+        "doing",
+        "done",
+        "into",
+        "than",
+        "too",
+        "very",
+        ".",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "-",
+        "",
+    ]
+
+    temp_list = [i for i in temp_list if i not in remove_list]
+    counter = collections.Counter(temp_list)
+    counter = counter.most_common(10)
+    print(counter)
     return {
         "statusCode": 200,
         "headers": {
@@ -49,5 +133,5 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Methods": "POST,GET,PUT,DELETE",
             "Access-Control-Allow-Headers": "Content-Type",
         },
-        "body": json.dumps(tweet_list),
+        "body": json.dumps({"tweetList": tweet_list, "counter": counter}),
     }
